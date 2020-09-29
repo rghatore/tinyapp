29f07cb6ac05c6094f136254a4c21b20051cb046
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-09-29 03:32:46 +0000

    added a new template for creating new short ulrs using a form. Still work in progress to make the short urls unique

--- express_server.js
+++ express_server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require('body-parser');
+
+// this will convert request body data from buffer to string we can read
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 // setting ejs as a template engine
 app.set('view engine', 'ejs');
@@ -9,6 +14,15 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// this function generates a random six string alphanumeric characters
+function generateRandomString() {
+  const result = Math.random().toString(36).substring(2,8);
+  // console.log(result);
+  return result;
+};
+
+generateRandomString();
 
 // sending a string
 app.get('/', (req, res) => {
@@ -32,9 +46,20 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+// routing to get the form for new urls
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+// submitting form at urls/new and posting response
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 // using ejs template + req.params to /urls:shortUrl route
-app.get('/urls/:shortUrl', (req, res) => {
-  const templateVars = { shortUrl: req.params.shortUrl, longUrl: urlDatabase[req.params.shortUrl] };
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longUrl: urlDatabase[req.params.shortURL] };
   // console.log(templateVars); // including this to understand the code - to be deleted later
   res.render('urls_show', templateVars);
 });
