f0deb568abfd9c004f9f5f4e11493626783bf8f6
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-09-29 01:09:11 +0000

    updated server with routing to /urls/shortUrl and created a urls_show.ejs template to display shortened urls individual page

--- express_server.js
+++ express_server.js
@@ -25,12 +25,19 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
-// using ejs templatet to /urls route
+// using ejs template to /urls route
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   // console.log(templateVars); // including this to understand the code - to be deleted later
   res.render('urls_index', templateVars);
-})
+});
+
+// using ejs template + req.params to /urls:shortUrl route
+app.get('/urls/:shortUrl', (req, res) => {
+  const templateVars = { shortUrl: req.params.shortUrl, longUrl: urlDatabase[req.params.shortUrl] };
+  // console.log(templateVars); // including this to understand the code - to be deleted later
+  res.render('urls_show', templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
