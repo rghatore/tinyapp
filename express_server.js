c114e646508123868327c7ca135d533af3097f3e
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-09-29 00:47:58 +0000

    updated server with routing to /urls and created a urls_index.ejs template to dispay shortened urls in a table

--- express_server.js
+++ express_server.js
@@ -25,6 +25,13 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
+// using ejs templatet to /urls route
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  // console.log(templateVars); // including this to understand the code - to be deleted later
+  res.render('urls_index', templateVars);
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
