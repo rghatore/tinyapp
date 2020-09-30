07f4dbf1e6ff8883bdd3f74420df5a9ee8810686
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-09-30 02:28:07 +0000

    added cookie-parser dependency and session cookie tracking functionality using login and logout

--- express_server.js
+++ express_server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const bodyParser = require('body-parser');
-const { response } = require('express');
+const { response } = require('express'); // what is this??
+const cookieParser = require('cookie-parser');
 
 // this will convert request body data from buffer to string we can read
 app.use(bodyParser.urlencoded({extended: true}));
+
+// this will populate req.cookies object in key-value pairs
+app.use(cookieParser());
 
 
 // setting ejs as a template engine
@@ -25,31 +29,36 @@ function generateRandomString() {
 
 // generateRandomString();
 
-// sending a string
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
+// sending a string - this was just a test
+// app.get('/', (req, res) => {
+//   res.send('Hello!');
+// });
 
 // sending a json string
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-// sending an html response
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
+// sending an html response - this was just a test
+// app.get('/hello', (req, res) => {
+//   res.send('<html><body>Hello <b>World</b></body></html>\n');
+// });
 
 // using ejs template to /urls route
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    username: req.cookies['username'],
+    urls: urlDatabase };
   // console.log(templateVars); // including this to understand the code - to be deleted later
   res.render('urls_index', templateVars);
 });
 
 // routing to get the form for new urls
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  const templateVars = {
+    username: req.cookies['username']
+  }
+  res.render('urls_new', templateVars);
 });
 
 // submitting form at urls/new and posting response
@@ -64,7 +73,10 @@ app.post("/urls", (req, res) => {
 
 // using ejs template + req.params to /urls:shortUrl route
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    username: req.cookies['username'],
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL] };
   // console.log(templateVars); // including this to understand the code - to be deleted later
   res.render('urls_show', templateVars);
 });
@@ -84,6 +96,18 @@ app.post('/urls/:shortURL', (req, res) => {
   // console.log(urlDatabase);
   res.redirect('/urls');
 });
+
+// Login with username and set cookie
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+})
+
+// Logout the user and clear cookie
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+})
 
 // Deleting a url entry
 app.post('/urls/:shortURL/delete', (req, res) => {
